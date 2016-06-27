a436494e37488bd3e9c0893c0144ee88921d08cf
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-06-27 17:11:45 -0400

    added user authentication

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ var fs = require('fs');
 var fsAccess = require('fs-access');
 var wikiLinkify = require('wiki-linkify');
 var marked = require('marked');
+var session = require('express-session');
 
 var app = express();
+var logFile = 'pages/filelog.txt';
+
+app.use(session({
+  secret: 'uheosahuhutnesohuntsoe',
+  cookie: {
+    maxAge: 24 * 3600000
+  }
+}));
+
+app.use(function(request, response, next) {
+  console.log('first');
+  console.log(request.method + ' ' + request.url);
+  fs.appendFile(logFile, request.method + ' ' + request.url + '\n');
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'hbs');
@@ -13,6 +29,30 @@ app.set('view engine', 'hbs');
 app.get('/', function(request, response) {
   response.redirect('/HomePage');
 });
+
+app.get('/login', function(request, response) {
+  response.render('login.hbs');
+});
+
+app.post('/login-submit', function(request, response) {
+  var credentials = request.body;
+  console.log(credentials);
+  if (credentials.username === 'Toby' && credentials.password === 'thepassword') {
+    request.session.user = credentials.username;
+    response.redirect(request.session.requestUrl);
+  } else {
+    response.redirect('/login');
+  }
+});
+
+function authRequired(request, response, next) {
+  if(!request.session.user) {
+    request.session.requestUrl = request.url;
+    response.redirect('/login');
+  } else {
+    next();
+  }
+}
 
 app.get('/AllPages', function(request, response) {
   var pageName = request.params.pageName;
@@ -67,9 +107,10 @@ app.get('/:pageName', function(request, response) {
   });
 });
 
-app.get('/:pageName/edit', function(request, response) {
+app.get('/:pageName/edit', authRequired, function(request, response) {
   //is user logged in? - if no, make sure he's logged in - redirect to log in page
   var pageName = request.params.pageName;
+  console.log(pageName);
   var filename = 'pages/' + pageName + '.md';
   fs.readFile(filename, function(err, data) {
     if (err) {
